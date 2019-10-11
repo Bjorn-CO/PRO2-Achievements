489fef9ecc8cc87135586a942e30c13ab12dd1d9
Bjornraaf <30087@ma-web.nl>
2019-10-11 11:08:14 +0200

    Dotnet toegevoegd

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubbertjee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
